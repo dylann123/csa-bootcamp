a2b59c882fe14a03754d64cba80edc20ea3d3b30
Firecreper2 <64336537+dylann123@users.noreply.github.com>
2023-08-28 23:22:06 -0400

    fin?

--- MyProgram.java
+++ MyProgram.java
@@ -1,23 +1,75 @@
+/*
+28 August 2023
+APCSA Bootcamp
+*/
+
 import java.util.Scanner; // allows us to use functions in the Scanner class
 							// these functions allow us to get input from the user
 
 class MyProgram {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in); // creates a new Scanner object
-												  // gets input from the System.in stream (the text console)
 
-		System.out.println("Hello World!"); // show hello world and go to the next line (println)
+		// creates a new Scanner object
+		// gets input from the System.in stream (the text console)
+		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("My name is: "); // show my name but stay on the same line (print)
+		// show "My name is: " and stay on the same line (print)
+		System.out.print("My name is: ");
 
-		String name = scanner.nextLine(); // get the next line of input from the user
-										  // and store it in the variable name
+		// get the next line of input from the user
+		// and store it in the variable 'name'
+		String name = scanner.nextLine();
 
-		System.out.println("Hello " + name + "!"); // combine strings with the + operator (concatenation)
-												   // and print the result 
+		// combine strings and the variable 'name' with the + operator (string
+		// concatenation/combining)
+		String greeting = "Hello " + name + "!";
 
-		
+		// show the greeting
+		System.out.println(greeting);
+
+		// print instead of println to
+		// stay on the same line
+		System.out.print("How old are you? ");
+
+		// get the next line of input from the user
+		// and store it in the variable 'age'
+		int age = scanner.nextInt();
+
+		/*
+		 * the == operator checks if two values are equal
+		 * ex. 5 == 5 is true, 5 == 6 is false
+		 * the % operator (modulus) returns the remainder of a division
+		 * ex. 5 % 2 == 1, 8 % 2 == 0
+		 * 
+		 * a boolean variable can only be true or false
+		 * the isEven variable will be true if age is divisible by 2, false otherwise
+		 */
+		boolean isEven = (age % 2 == 0);
+
+		/*
+		 * an if statement checks if a given condition returns true
+		 * ex. if (age > 10) { ... } checks if age is greater than 10
+		 * 
+		 * isEven is a boolean variable, so we can use it in an if statement
+		 */
+		if (isEven) {
+			// combining strings and integers works!
+			System.out.println("Your age, " + age + " is even!");
+		} else {
+			System.out.println("Your age, " + age + " is odd!");
+		}
+
+		// dividing an int by an int will return an int
+		// the result will be truncated to the decimal point / rounded down to the nearest integer
+		int intAge = age / 7;
+		System.out.println(age + " int divided by 7 is " + intAge);
+
+		// dividing a double by an int OR double will return a double (decimal number)
+		// doubles will alawys have a decimal point
+		double doubleAge = age / 7.0;
+		System.out.println(age + " double divided by 7 is " + doubleAge);
 
 		scanner.close();
+		// close the scanner to prevent memory leaks
 	}
 }
